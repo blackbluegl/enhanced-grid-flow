3b60f173265b7daa59dbd687bf65542e9d5c0ac2
Lutz Guettler <lutz.guettler@ascon-systems.de>
2024-12-06 09:04:08 +0100

    Workaround missing filtericon after detach/attach

--- enhanced-grid-flow-demo/src/main/java/com/vaadin/componentfactory/enhancedgrid/DialogExampleView.java
+++ enhanced-grid-flow-demo/src/main/java/com/vaadin/componentfactory/enhancedgrid/DialogExampleView.java
@@ -4,8 +4,11 @@ import java.util.List;
 
 import com.vaadin.componentfactory.enhancedgrid.bean.Person;
 import com.vaadin.componentfactory.enhancedgrid.filtering.TextFilterField;
+import com.vaadin.flow.component.AttachEvent;
+import com.vaadin.flow.component.HasValueAndElement;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.dialog.Dialog;
+import com.vaadin.flow.component.grid.FilterFieldDto;
 import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.Paragraph;
 import com.vaadin.flow.component.icon.VaadinIcon;
@@ -22,11 +25,24 @@ public class DialogExampleView extends Div {
         add(new Paragraph("Opens the grid in a dialog"));
 
         Dialog dialog = new Dialog();
-        EnhancedGrid<String> credits = new EnhancedGrid<>();
-        credits.addColumn(o -> o).setHeader("col1", new TextFilterField());
-        credits.setItems(List.of("data1", "data2"));
-        credits.setFilterIcon(VaadinIcon.FILTER.create());
-        dialog.add(credits);
+        EnhancedGrid<String> grid = new EnhancedGrid<>() {
+            @Override
+            protected void onAttach(AttachEvent attachEvent) {
+                super.onAttach(attachEvent);
+                getColumns().forEach(column -> {
+                    EnhancedColumn<String> enhancedColumn = (EnhancedColumn<String>) column;
+                    @SuppressWarnings("rawtypes")
+                    HasValueAndElement<?, ? extends FilterFieldDto> filter = enhancedColumn.getFilter();
+
+                    enhancedColumn.setHeader("col1", filter);
+                });
+                setFilterIcon(VaadinIcon.FILTER.create());
+            }
+        };
+        grid.addColumn(o -> o).setHeader("col1", new TextFilterField());
+        grid.setItems(List.of("data1", "data2"));
+        grid.setFilterIcon(VaadinIcon.FILTER.create());
+        dialog.add(grid);
         dialog.setWidth("800px");
         dialog.setWidth("600px");
         Button button = new Button("Open dialog", e -> dialog.open());
